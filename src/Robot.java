4ecd0ecc77db272155d3ceb8526ad26ae6fbefa2
unknown <pistorij@i5-61.ens-epua.univ-savoie.fr>
2019-03-08 09:01:57 +0100

    Initial commit

--- src/Robot.java
+++ src/Robot.java
@@ -134,26 +134,6 @@ public class Robot implements Runnable
             omniDrive.setVelocity(0, 0, 90*rotation);         
         }
     }
-    
-    /*
-    protected void circle() throws InterruptedException{
-    	float[] dir;
-        float a = 0.0f;
-        long startTime = System.currentTimeMillis();
-        int millisecondsElapsed = 0;
-        while (!Thread.interrupted() && com.isConnected() && false == bumper.value())
-        {
-			long elapsedTime = System.currentTimeMillis() - startTime;
-        
-            //rotate by 360 degrees every 10 seconds
-            dir = rotate(startVector, a);
-            a = 360.0f * elapsedTime / 10000;
-            
-            
-            omniDrive.setVelocity(dir[0], dir[1], 0);
-        }
-    }*/
-    
     protected void burn(){
     	while (!Thread.interrupted() && com.isConnected() && false == bumper.value()){
             omniDrive.setVelocity(0, 0, 90);
@@ -205,56 +185,5 @@ public class Robot implements Runnable
 		} catch (JSONException e1) {
 			e1.printStackTrace();
 		}
-    }
-
-    
-    
-    
-    //A METTRE DANS PROD_RPI
-    public void modeAuto(){
-    	//example creating graph and fastest path
-    	ArrayList<Vertex> v=new ArrayList<Vertex>();
-		v.add(new Vertex("SalleAPP1",1));
-		v.add(new Vertex("SalleAPP2",2));
-		v.add(new Vertex("SortieSalleAPP",3));
-		v.add(new Vertex("SortieSalleGauche",4));
-		v.add(new Vertex("SortieSalleDroite",5));
-
-		ArrayList<Edge> e=new ArrayList<Edge>();
-		e.add(new Edge(7,v.get(0),v.get(1)));
-		e.add(new Edge(9,v.get(1),v.get(2)));
-		e.add(new Edge(14,v.get(2),v.get(3)));
-		e.add(new Edge(10,v.get(2),v.get(4)));
-
-		ShortestPath sp=new ShortestPath(new Map(v,e),v.get(0));
-		sp.getAllPaths();
-		sp.getPath(v.get(4));
-		
-		
-		//example pathCycle
-		ArrayList<Vertex> regularPath=new ArrayList<Vertex>();
-		regularPath.add(v.get(0));
-		regularPath.add(v.get(1));
-		regularPath.add(v.get(2));
-		regularPath.add(v.get(3));
-		regularPath.add(v.get(2));
-		regularPath.add(v.get(4));
-		regularPath.add(v.get(2));
-		regularPath.add(v.get(1));
-		System.out.println(pathCycle(v.get(0),regularPath));
-    }
-    //need to remove regularPath from parameter
-    public Vertex pathCycle(Vertex vCurrent,ArrayList<Vertex> regularPath){
-    	for(Vertex v:regularPath){
-    		if(v.equals(vCurrent)){
-    			if(regularPath.indexOf(v)==regularPath.size()-1){
-    				return regularPath.get(0);
-    			}else{
-    				return regularPath.get(regularPath.indexOf(v));
-    			}
-    		}
-    	}
-		return vCurrent;
-    }
-    
+    } 
 }
